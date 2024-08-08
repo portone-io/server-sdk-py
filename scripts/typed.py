5020984180f6f62db90b0684eac71b73c7fab8f6
kiwiyou <kiwiyou@portone.io>
2024-08-08 11:18:57 +0900

    chore: remove typed gen

--- scripts/typed.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-
-def gen() -> None:
-    root = Path(__file__).parent.parent.joinpath("./portone_server_sdk")
-    for path in root.glob("**/"):
-        with open(path.joinpath("./py.typed"), "wb") as f:
-            f.write(b"\n")
